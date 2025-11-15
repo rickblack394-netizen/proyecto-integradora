5c19062c5d3c713f68b37349949f723e46b5ab46
rickblack394-netizen <rickblack394@gmail.com>
2025-11-15 16:06:56 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+
+const CACHE_NAME = "ecoBeauty-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/logo.jpeg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.map(name => {
+        if (name !== CACHE_NAME) return caches.delete(name);
+      }))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
